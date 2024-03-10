1d68e9f0378ce459616f98d09d8bcc6279748df2
lmillan1 <130090548+lmillan1@users.noreply.github.com>
2024-03-09 19:39:45 -0800

    Initial Commit

---
+++ CSE1933/CSE1933App.swift
@@ -0,0 +1,17 @@
+//
+//  CSE1933App.swift
+//  CSE1933
+//
+//  Created by Luis Millan on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CSE1933App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSE1933/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CSE1933
+//
+//  Created by Luis Millan on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
